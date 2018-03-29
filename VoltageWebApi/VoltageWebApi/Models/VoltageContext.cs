5051e6fd7bb8604c9f9eceb1c7c53974875afdaa
Bharath Bhushan M <mbharath1983@gmail.com>
2018-03-29 19:24:33 +0530

    WebAPI Sample
    WebAPI Sample

---
+++ VoltageWebApi/VoltageWebApi/Models/VoltageContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace VoltageWebApi.Models
+{
+    public class VoltageContext : DbContext
+    {
+        public VoltageContext(DbContextOptions<VoltageContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
